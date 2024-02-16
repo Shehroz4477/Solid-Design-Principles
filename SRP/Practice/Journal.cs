c01725cc41582866dc4cc4750aed01d0278b9ca1
SSDShehroz <shehroz@halimsol.com>
2024-02-16 15:23:10 +0500

    Voilation Single Responsibilty Principle

---
+++ SRP/Practice/Journal.cs
@@ -0,0 +1,38 @@
+﻿namespace SRP.Practice;
+
+public class Journal
+{
+    private readonly List<string> _entries = new List<string>();
+    public static int count = 0;
+
+    public int AddEntry(string entry)
+    {
+        _entries.Add($"{++count}:{entry}");
+        return count; // momento
+    }
+
+    public void RemoveEntry(int index)
+    {
+        _entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, _entries);
+    }
+
+    public void Save(string fileName)
+    {
+        File.WriteAllText(fileName, ToString);
+    }
+
+    public static Journal Load(string fileName)
+    {
+        //TODO
+    }
+
+    public void Load(Uri uri)
+    {
+        //TODO
+    }
+}
