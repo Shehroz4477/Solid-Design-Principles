7ae845c1a2769690a8861bc6036866e347331548
SSDShehroz <shehroz@halimsol.com>
2024-02-20 08:32:33 +0500

    Product Class Define OCP

---
+++ OCP/Practice/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace OCP.Practice;
+
+public class Product
+{
+    public string Name;
+    public Color Color;
+    public Size Size;
+    public Product(string name, Color color, Size size)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+        Name = name;
+        Color = color;
+        Size = size;
+    }
+}
