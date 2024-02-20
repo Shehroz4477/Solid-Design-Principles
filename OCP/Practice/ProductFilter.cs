e10bdd68b92ef3deedf496d286ca819de8e4cce1
SSDShehroz <shehroz@halimsol.com>
2024-02-20 14:44:23 +0500

    Filter By Color Add in ProductFilter Class

--- OCP/Practice/ProductFilter.cs
+++ OCP/Practice/ProductFilter.cs
@@ -1,14 +1,26 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace OCP.Practice;
 
 public class ProductFilter
 {
-    public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+    public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
     {
         foreach (Product product in products)
         {
             if(product.Size == size)
+            {
+                yield return product;
+            }
+        }
+    }
+
+    public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+    {
+        foreach (Product product in products)
+        {
+            if (product.Color == color)
             {
                 yield return product;
             }
