f47f7253d822df934af81d233ffe8342139689b2
SSDShehroz <shehroz@halimsol.com>
2024-02-20 18:46:36 +0500

    AndSpecification Define For Both Color and Size Filter

---
+++ OCP/Practice/AndSpecification.cs
@@ -0,0 +1,19 @@
+﻿using OCP.Practice.Interfaces;
+
+namespace OCP.Practice;
+
+public class AndSpecification<Type> : ISpecification<Type>
+{
+    ISpecification<Type> _first, _second;
+
+    public AndSpecification(ISpecification<Type> first, ISpecification<Type> second)
+    {
+        _first = first?? throw new ArgumentNullException(nameof(first));
+        _second = second?? throw new ArgumentNullException(nameof(second));
+    }
+
+    public bool IsSatisfied(Type item)
+    {
+        return (_first.IsSatisfied(item) && _second.IsSatisfied(item));
+    }
+}
