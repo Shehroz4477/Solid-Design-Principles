119a5ec211e8b22e15170306c3b41e440b6bc95c
SSDShehroz <shehroz@halimsol.com>
2024-02-20 09:07:27 +0500

    Product Size Filter Static Method Define in ProductFilter Class

---
+++ OCP/Practice/ProductFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace OCP.Practice;
+
+public class ProductFilter
+{
+    public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+    {
+        foreach (Product product in products)
+        {
+            if(product.Size == size)
+            {
+                yield return product;
+            }
+        }
+    }
+}
